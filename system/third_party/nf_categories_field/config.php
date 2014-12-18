02c2c13b202c6e29774b9ec8ba454788905925d5
Nathan Pitman <nathan@ninefour.co.uk>
2014-12-18 17:27:03 +0000

    Increment version number

--- system/third_party/nf_categories_field/config.php
+++ system/third_party/nf_categories_field/config.php
@@ -12,7 +12,7 @@
 if ( ! defined('NF_CF_NAME'))
 {
     define('NF_CF_NAME',    'Categories Field');
-    define('NF_CF_VERSION', '1.1.3');
+    define('NF_CF_VERSION', '1.1.4');
     define('NF_CF_DOCS',    'http://github.com/ninefour/categories_field.ft.ee_addon');
 }
 
